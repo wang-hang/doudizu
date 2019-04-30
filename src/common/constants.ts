a509cf82773791a846a09cc5efa940fea8f1317d
wanghang <wanghang@vipkid.com.cn>
2019-04-30 11:28:54 +0800

    删除无用代码

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,11 +1,3 @@
-// import { POKER } from './commonTypes'
-
-// export const meihua = '♣️' as POKER.meihua
-// export const fangpian = '♦️' as POKER.fangpian
-// export const hongtao = '♣️' as POKER.hongtao
-// export const heitao = '♣♠️' as POKER.heitao
-
-
 export enum POKER{
   meihua = "♣",
   fangpian = "️️️️️️♦️",
